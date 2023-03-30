13af0d98e649a7ffdcab7f9314d403a2d3f58919
jankettler <jankettler@cippy01.cogsci.uni-osnabrueck.de>
2023-03-30 19:29:59 +0200

    modify specifactions before trying without mpi

--- large-scale-sparse-neural-networks/super_run.sh
+++ large-scale-sparse-neural-networks/super_run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cluster_cmd="qsub -v -b -cwd -pe default 2 -l cpu=2,num_proc=2,mem=2G ./run.sh" 
+cluster_cmd="qsub -V -b y -now y -cwd -pe mpi 2 -l mem=2G ./run.sh"
+#cpu=2,num_proc=2,
 echo "the following command was executed in super_run.sh followed by a seed(1-5)"
 echo $cluster_cmd
 
