b94c0a1d1f107679d0065ecedda2ab841f58e774
jankettler <jankettler@cippy01.cogsci.uni-osnabrueck.de>
2023-03-30 14:00:11 +0200

    work on runfiles with not enough cpus issue

--- large-scale-sparse-neural-networks/super_run.sh
+++ large-scale-sparse-neural-networks/super_run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-cluster_cmd="qsub -v -b -cwd -l mem=8G,cuda=1 ./run.sh" 
+cluster_cmd="qsub -v -b -cwd -l cpu=4,num_proc=4,mem=2G ./run.sh" 
+echo "the following command was executed in super_run.sh followed by a seed(1-5)"
+echo $cluster_cmd
 
 for seed in 1; do $cluster_cmd $seed; done
