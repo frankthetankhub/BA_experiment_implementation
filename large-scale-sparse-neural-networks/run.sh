1642d1ff58f5ac9e0832c315791b032c7864d40e
jankettler <jankettler@cippy04.cogsci.uni-osnabrueck.de>
2023-04-03 19:33:27 +0200

    update runfiles

--- large-scale-sparse-neural-networks/run.sh
+++ large-scale-sparse-neural-networks/run.sh
@@ -1,20 +1,10 @@
-#!/bin/bash
-
-ARGS="--monitor --epochs 10 --processes 2 --n-neurons 784 --n-neurons 1000 --n-neurons 1000 --n-neurons 1000 --n-neurons 10"
-location="/net/projects/scratch/summer/valid_until_31_January_2024/jankettler/Ba/large-scale-sparse-neural-networks/parallel_training.py"
-
-python_loc="/net/projects/scratch/summer/valid_until_31_January_2024/jankettler/python/bin/python3.7"
-
-#echo mpiexec -n 6 python parallel_training.py $ARGS 
-
-python_cmd="$HOME/scratch/openmpi/bin/mpiexec -n 2 --report-bindings $python_loc $location"
-no_mpi_command="python parallel_training.py"
-#for SEED in 1 2 3 4 5; do echo $cmd $ARGS --seed $SEED; done
-
-echo "First argument passed should be seed. Its value is: $1"
-echo "Check CPU with -lscpu-"
-lscpu
-echo $LD_LIBRARY_PATH
-echo $PATH
-$python_cmd $ARGS --seed $1
-# $no_mpi_command $ARGS --seed $1
+# This is the runfile for not submitting jobs
+ARGS="--monitor --epochs 10 --processes 6 --n-neurons 784 --n-neurons 1000 --n-neurons 1000 --n-neurons 1000 --n-neurons 10"
+echo "Seed: $1"
+sas="/net/projects/scratch/summer/valid_until_31_January_2024/jankettler/pdm_venvs/large-scale-sparse-neural-networks-yLgfzVYs-3.7/bin/python"
+p="/scratch/pdm_venvs/large-scale-sparse-neural-networks-yLgfzVYs-3.7/bin/python3.7"
+python="/net/projects/scratch/summer/valid_until_31_January_2024/jankettler/python/Python-3.7.16/python"
+cwd="/home/student/j/jankettler/scratch/Ba/large-scale-sparse-neural-networks"
+cmd="mpiexec -n 6 $sas $cwd/parallel_training.py $ARGS --seed $1"
+echo $cmd
+$cmd
--- large-scale-sparse-neural-networks/super_run.sh
+++ large-scale-sparse-neural-networks/super_run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cluster_cmd="qsub -V -now y -cwd -pe default 2 -l mem=2G ./run.sh"
+output_location="$HOME/scratch/Ba/large-scale-sparse-neural-networks/cluster_outputfiles/"
+cluster_cmd="qsub -V -now y -cwd -o $output_location -e $output_location -pe default 2 -l mem=2G ./run_cluster.sh"
 #cpu=2,num_proc=2,
 echo "the following command was executed in super_run.sh followed by a seed(1-5)"
 echo $cluster_cmd
