e35560ca883064e94e64a7e826cf5282d372fbd0
Jan <jan.kettler99@gmail.com>
2023-04-07 16:20:02 +0200

    prepare for testrun submit

--- large-scale-sparse-neural-networks/test.sh
+++ large-scale-sparse-neural-networks/test.sh
@@ -9,9 +9,10 @@ for FILE in configs/*;
     p="/scratch/pdm_venvs/large-scale-sparse-neural-networks-yLgfzVYs-3.7/bin/python3.7"
     python="/net/projects/scratch/summer/valid_until_31_January_2024/jankettler/python/Python-3.7.16/python"
     cwd="/home/student/j/jankettler/scratch/Ba/large-scale-sparse-neural-networks"
-    cmd="mpiexec -n 6 $sas $cwd/parallel_training.py $ARGS --config_file $FILE"
+    cmd_alt="mpiexec -n 6 $sas $cwd/parallel_training.py $ARGS --config_file $FILE"
+    cmd="mpiexec -n 6 pdm run python parallel_training.py $ARGS --config_file $FILE"
     echo $cmd
     echo "testing for seed loop"
-    for SEED in {1..5}; do echo $cmd --seed $SEED; done
-    #$cmd
+    for SEED in {1..2}; do echo $cmd --seed $SEED; done
+    for SEED in {1..2}; do $cmd --seed $SEED; done
 done 
