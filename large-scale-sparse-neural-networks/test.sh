9076aa3071999890c97589b146be443136c1a014
Jan <jan.kettler99@gmail.com>
2023-04-08 15:37:30 +0200

    add selection of cv hosts so no runtime errors because of glibc hopefully

--- large-scale-sparse-neural-networks/test.sh
+++ large-scale-sparse-neural-networks/test.sh
@@ -6,6 +6,8 @@ for FILE in configs/*;
     full_name=$FILE
     base_name=$(basename ${full_name})
     echo ${base_name}
+    cv_hosts="albireo alioth beam bias cujam dimension gremium light nashira perception rigel shadow twilight vector voxel"
+    host_commands="h=albireo,h=alioth,h=beam,h=biash,h=cujam,h=dimension,h=gremium,h=light,h=nashira,h=perception,h=rigel,h=shadow,h=twilight,h=vector,h=voxel"
     ARGS=$(cat $FILE)
     #sas="/net/projects/scratch/summer/valid_until_31_January_2024/jankettler/pdm_venvs/large-scale-sparse-neural-networks-yLgfzVYs-3.7/bin/python"
     #p="/scratch/pdm_venvs/large-scale-sparse-neural-networks-yLgfzVYs-3.7/bin/python3.7"
@@ -14,7 +16,7 @@ for FILE in configs/*;
     #cmd_alt="mpiexec -n 6 $sas $cwd/parallel_training.py $ARGS --config_file $base_name"
     cmd="mpiexec -n 6 pdm run python parallel_training.py $ARGS --config_file $base_name"
     cluster_cmd="qsub -b y -V -l mem=8G -cwd -pe default 6"
-    cluster_cmd_cifar="qsub -b y -V -l mem=14G -cwd -pe default 6"
+    cluster_cmd_cifar="qsub -b y -V -l mem=14G,$host_commands -cwd -pe default 6"
     echo $cmd
     if [[ $base_name == cifar* ]];
     then
@@ -22,7 +24,7 @@ for FILE in configs/*;
         for SEED in {1..2}; do echo $cluster_cmd_cifar $cmd --seed $SEED; done
         for SEED in {1..2}; do $cluster_cmd_cifar $cmd --seed $SEED; done
     else
-        for SEED in {1..2}; do echo $cluster_cmd $cmd --seed $SEED; done
-        for SEED in {1..2}; do $cluster_cmd $cmd --seed $SEED; done
+        for SEED in {1..2}; do echo $cluster_cmd -N ${base_name}_seed$SEED $cmd --seed $SEED; done
+        for SEED in {1..2}; do $cluster_cmd -N ${base_name}_seed$SEED $cmd --seed $SEED; done
     fi
 done 
