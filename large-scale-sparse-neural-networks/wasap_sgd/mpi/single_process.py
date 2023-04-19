72b32e59751a119e40f81f10f56d9256175b449f
Jan <jan.kettler99@gmail.com>
2023-04-19 18:49:35 +0200

    fix single worker training

--- large-scale-sparse-neural-networks/wasap_sgd/mpi/single_process.py
+++ large-scale-sparse-neural-networks/wasap_sgd/mpi/single_process.py
@@ -29,38 +29,47 @@ class MPISingleWorker(MPIWorker):
         #save model weight init
         np.savez_compressed(self.save_filename + "_initial_weights.npz", self.model.get_weights()['w'])
         np.savez_compressed(self.save_filename + "_initial_biases.npz", self.model.get_weights()['b'])
+        batches_per_epoch = self.data.get_train_data().shape[0] // self.data.batch_size
         for epoch in range(1, self.num_epochs + 1):
             logging.info("beginning epoch {:d}".format(self.epoch + epoch))
             if self.monitor:
                 self.monitor.start_monitor()
+
+            num_batches = 0
             start_train = datetime.datetime.now()
-            for j in range(self.data.x_train.shape[0] // self.data.batch_size):#wichtige stelle hier startet der trainingsprocess für single_process bzw parallel training
-                start_pos = j * self.data.batch_size
-                end_pos = (j + 1) * self.data.batch_size
-                batch = self.data.x_train[start_pos:end_pos], self.data.y_train[start_pos:end_pos]
-                self.update = self.model.train_on_batch(x=batch[0], y=batch[1])
+            for x_b, y_b in self.data.generate_data():#wichtige stelle hier startet der trainingsprocess für single_process bzw parallel training
+                num_batches += 1
+                self.update = self.model.train_on_batch(x=x_b, y=y_b)
 
                 self.model.apply_update(self.update)
+                if num_batches % batches_per_epoch == 0:
+                    break
+            
             end_train = datetime.datetime.now()
             trainig_time = (end_train - start_train).total_seconds()
+
             if self.monitor:
                 self.monitor.stop_monitor()
 
             if testing:
                 t3 = datetime.datetime.now()
-                accuracy_test, activations_test = self.model.predict(self.data.x_test, self.data.y_test)
-                accuracy_train, activations_train = self.model.predict(self.data.x_train, self.data.y_train)
+                accuracy_test, activations_test = self.model.predict(self.data.get_test_data(), self.data.get_test_labels())
+                accuracy_train, activations_train = self.model.predict(self.data.get_train_data(), self.data.get_train_labels())
+                # accuracy_test, activations_test = self.model.predict(self.data.x_test, self.data.y_test)
+                # accuracy_train, activations_train = self.model.predict(self.data.x_train, self.data.y_train)
                 t4 = datetime.datetime.now()
                 self.maximum_accuracy = max(self.maximum_accuracy, accuracy_test)
-                loss_test = self.model.compute_loss(self.data.y_test, activations_test)
-                loss_train = self.model.compute_loss(self.data.y_train, activations_train)
+                # loss_test = self.model.compute_loss(self.data.y_test, activations_test)
+                # loss_train = self.model.compute_loss(self.data.y_train, activations_train)
+                loss_test = self.model.compute_loss(self.data.get_test_labels(), activations_test)
+                loss_train = self.model.compute_loss(self.data.get_train_labels(), activations_train)
                 metrics[epoch-1, 0] = loss_train
                 metrics[epoch-1, 1] = loss_test
                 metrics[epoch-1, 2] = accuracy_train
                 metrics[epoch-1, 3] = accuracy_test
                 testing_time = (t4 - t3).total_seconds()
                 self.logger.info(f"Training time: {trainig_time};")
-                self.logger.info(f"Testing time: {testing_time};\nLoss train: {loss_train}; Loss test: {loss_test}; \n"
+                self.logger.info(f"Testing time: {testing_time}; \nLoss train: {loss_train}; Loss test: {loss_test}; \n"
                                  f"Accuracy train: {accuracy_train}; Accuracy test: {accuracy_test}; \n"
                                  f"Maximum accuracy test: {self.maximum_accuracy}")
                 self.validate_time += testing_time
@@ -74,7 +83,6 @@ class MPISingleWorker(MPIWorker):
             if epoch % self.save_weight_interval == 0:
                 weights.append(self.model.get_weights()['w'].copy())
                 biases.append(self.model.get_weights()['b'].copy()) #wichtige stelle da hier potentiell das saven von weights etc angebracht ist
-            
             if epoch < self.num_epochs - 1:  # do not change connectivity pattern after the last epoch
                 start_evolution = datetime.datetime.now()
                 self.model.weight_evolution(epoch) #wichtige stelle da hier weight evolution durchgeführt wird
@@ -93,12 +101,12 @@ class MPISingleWorker(MPIWorker):
 
     def validate(self):
         t3 = datetime.datetime.now()
-        accuracy_test, activations_test = self.model.predict(self.data.x_test, self.data.y_test)
-        accuracy_train, activations_train = self.model.predict(self.data.x_train, self.data.y_train)
+        accuracy_test, activations_test = self.model.predict(self.data.get_test_data(), self.data.get_test_labels())
+        accuracy_train, activations_train = self.model.predict(self.data.get_train_data(), self.data.get_train_labels())
         t4 = datetime.datetime.now()
         self.maximum_accuracy = max(self.maximum_accuracy, accuracy_test)
-        loss_test = self.model.compute_loss(self.data.y_test, activations_test)
-        loss_train = self.model.compute_loss(self.data.y_train, activations_train)
+        loss_test = self.model.compute_loss(self.data.get_test_labels(), activations_test)
+        loss_train = self.model.compute_loss(self.data.get_train_labels(), activations_train)
         t_5 = (t4-t3).total_seconds()
         self.logger.info(f"Testing time: {t_5}; \nLoss train: {loss_train}; Loss test: {loss_test}; \n"
                          f"Accuracy train: {accuracy_train}; Accuracy test: {accuracy_test}; \n"
